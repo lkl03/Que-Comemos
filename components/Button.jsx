184f6ba09eca318b4f129fd81544077e8629f99e
Luca <lulago17@gmail.com>
2022-02-24 06:28:53 +1200

    pruebas 01

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,31 @@
 import React, {useState} from 'react'
 import { FaCog } from 'react-icons/fa'
 
+export async function getServerSideProps() {
+    const res = await fetch(`https://animechan.vercel.app/api/random`)
+    const data = await res.json()
+  
+    return {
+      props: {
+        quote: data.quote,
+        anime: data.anime,
+        character: data.character
+      }
+    }
+  }
+
 const Button = () => {
   
     const [clicked, setClicked] = useState(false)
 
-    const handleClicked = () => {
+    const handleClicked = (props) => {
+        return (
+            <div>
+                <p style={{fontWeight: 'bold'}}>{props.anime}</p>
+                <p>{props.quote}</p>
+                <p style={{fontWeight: 'bold'}}>-{props.character}</p>
+            </div>
+        )
         setClicked(!clicked)
     }
 
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,30 @@ import Search from '../components/Search'
 import Button from '../components/Button'
 import InfoButtons from '../components/InfoButtons'
 import Footer from '../components/Footer'
+import { useState } from 'react'
+import { FaCog } from 'react-icons/fa'
 
-export default function Home() {
+export async function getServerSideProps() {
+  const res = await fetch(`https://animechan.vercel.app/api/random`)
+  const data = await res.json()
+
+  return {
+    props: {
+      quote: data.quote,
+      anime: data.anime,
+      character: data.character
+    }
+  }
+}
+
+export default function Home(props) {
+
+  const [clicked, setClicked] = useState(false)
+
+    const handleClicked = (props) => {
+      setClicked(!clicked)
+    }
+
   return (
     <>
       <Head>
@@ -15,10 +37,21 @@ export default function Home() {
       <div className="container">
         <Logo />
         <Search />
-        <Button />
+        <div className="main_button">
+        <p>¿O estás sin ideas? ¡Te ayudamos!</p>
+        <button onClick={handleClicked}>{clicked ? 'Preparando...' : 'Generar receta '} <FaCog className={clicked ? 'main_button-spinner' : null}/> </button>
+        {clicked ? 
+          <div>
+                <p style={{fontWeight: 'bold'}}>{props.anime}</p>
+                <p>{props.quote}</p>
+                <p style={{fontWeight: 'bold'}}>-{props.character}</p>
+            </div> : ''}
+        </div>
         <InfoButtons />
         <Footer />
       </div>
     </>
   )
 }
+
+
