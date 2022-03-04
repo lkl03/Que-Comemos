1287be1cdf90503cc891cd73915fa4c1d9d3126c
Luca <lulago17@gmail.com>
2022-03-05 05:32:01 +1100

    pruebas 02

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,38 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaCog } from 'react-icons/fa'
 
-export async function getServerSideProps() {
-    const res = await fetch(`https://animechan.vercel.app/api/random`)
-    const data = await res.json()
-  
-    return {
-      props: {
-        quote: data.quote,
-        anime: data.anime,
-        character: data.character
-      }
-    }
-  }
-
 const Button = () => {
-  
-    const [clicked, setClicked] = useState(false)
-
-    const handleClicked = (props) => {
-        return (
-            <div>
-                <p style={{fontWeight: 'bold'}}>{props.anime}</p>
-                <p>{props.quote}</p>
-                <p style={{fontWeight: 'bold'}}>-{props.character}</p>
-            </div>
-        )
-        setClicked(!clicked)
-    }
-
     return (
     <div className="main_button">
-        <p>¿O estás sin ideas? ¡Te ayudamos!</p>
-        <button onClick={handleClicked}>{clicked ? 'Preparando...' : 'Generar receta '} <FaCog className={clicked ? 'main_button-spinner' : null}/> </button>
+      <button />
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import Search from '../components/Search'
 import Button from '../components/Button'
 import InfoButtons from '../components/InfoButtons'
 import Footer from '../components/Footer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaCog } from 'react-icons/fa'
 
-export async function getServerSideProps() {
+{/*export async function getServerSideProps() {
   const res = await fetch(`https://animechan.vercel.app/api/random`)
   const data = await res.json()
 
@@ -19,9 +19,30 @@ export async function getServerSideProps() {
       character: data.character
     }
   }
-}
+}*/}
 
-export default function Home(props) {
+export default function Home() {
+
+  const [quote, setQuote] = useState({
+    anime: null,
+    character: null,
+    quote: null
+  })
+
+  const fetchQuote = async () => {
+    return await fetch('https://animechan.vercel.app/api/random')
+      .then(response => response.json())
+  }
+
+  const generate = async () => {
+    setClicked(!clicked)
+    setQuote(await fetchQuote())
+  }
+
+  useEffect(async () => {
+    setQuote(await fetchQuote())
+  }, [])
+
 
   const [clicked, setClicked] = useState(false)
 
@@ -35,17 +56,19 @@ export default function Home(props) {
         <title>Qué Comemos?</title>
       </Head>
       <div className="container">
-        <Logo />
+        {/*<Logo />*/}
         <Search />
         <div className="main_button">
-        <p>¿O estás sin ideas? ¡Te ayudamos!</p>
-        <button onClick={handleClicked}>{clicked ? 'Preparando...' : 'Generar receta '} <FaCog className={clicked ? 'main_button-spinner' : null}/> </button>
-        {clicked ? 
-          <div>
-                <p style={{fontWeight: 'bold'}}>{props.anime}</p>
-                <p>{props.quote}</p>
-                <p style={{fontWeight: 'bold'}}>-{props.character}</p>
-            </div> : ''}
+          <p>¿O estás sin ideas? ¡Te ayudamos!</p>
+          <button onClick={generate}>{clicked && quote ? 'Preparando...' : 'Generar receta '} <FaCog className={clicked ? 'main_button-spinner' : null}/> </button>
+          {clicked ? 
+              <div>
+                <p style={{fontWeight: 'bold'}}>{quote.anime}</p>
+                <p>{quote.quote}</p>
+                <p style={{fontWeight: 'bold'}}>-{quote.character}</p>
+              </div> 
+            : ''
+          }
         </div>
         <InfoButtons />
         <Footer />
